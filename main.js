28b47da1d6c0d52b90a448daa37cb2b6ca2e7ead
Dongwook Lim <ldu9905@gmail.com>
2023-01-12 12:40:24 +0900

    test

--- main.js
+++ main.js
@@ -16,20 +16,37 @@ idBtn.addEventListener('click', () => {
   darkScreen.style.zIndex = 0;
   darkScreen.style.animation = 'opacity1 250ms ease';
   setTimeout(() => {
+    darkScreen.style.zIndex = -1;
+    setTimeout(() => {
+      img.src = 'img/certification_dong.jpg';
+      img.style.animation = 'transform 300ms ease';
+      setTimeout(() => {
+        yearMonthBox.innerText =
+          `${year < 10 ? '0' + year : year}` +
+          '.' +
+          '  ' +
+          `${month < 10 ? '0' + month : month}` +
+          '.';
+        dayBox.innerText = `${day < 10 ? '0' + day : day}` + '.';
+        dateBox.style.animation = 'opacity2 500ms ease';
+      }, 200);
+    }, 200);
+  }, 0);
+  setTimeout(() => {
     img.src = 'img/certification_dong.jpg';
     darkScreen.style.zIndex = -1;
     img.style.animation = 'transform 300ms ease';
-  }, 250);
-  setTimeout(() => {
-    yearMonthBox.innerText =
-      `${year < 10 ? '0' + year : year}` +
-      '.' +
-      '  ' +
-      `${month < 10 ? '0' + month : month}` +
-      '.';
-    dayBox.innerText = `${day < 10 ? '0' + day : day}` + '.';
-    dateBox.style.animation = 'opacity2 300ms ease';
-  }, 300);
+    setTimeout(() => {
+      yearMonthBox.innerText =
+        `${year < 10 ? '0' + year : year}` +
+        '.' +
+        '  ' +
+        `${month < 10 ? '0' + month : month}` +
+        '.';
+      dayBox.innerText = `${day < 10 ? '0' + day : day}` + '.';
+      dateBox.style.animation = 'opacity2 500ms ease';
+    }, 0);
+  }, 0);
 });
 
 certiBtn.addEventListener('click', () => {
